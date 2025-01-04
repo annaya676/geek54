c13d08d6d42b0c59904edd0514d5866d33680f84
“sumit” <sumit.ducat@gmail.com>
2025-01-04 14:40:01 +0530

    second

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const database_connectivity=async ()=>{
  try{
-    await mongoose.connect(process.env.DB_CONNECTION);
+    await mongoose.connect("mongodb+srv://nikhilchaurasiya981:FeCM0qv5dUDd2NN2@cluster0.9kziy.mongodb.net/Cluster0?retryWrites=true&w=majority&appName=Cluster0");
     console.log("MongoDb connected");
  }
  catch(err){
